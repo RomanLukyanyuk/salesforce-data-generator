c2dc011606c9785fe8bb7de943f0a9a0b670f878
Roman Lukyanyuk <ramonak.luk@gmail.com>
2017-01-10 12:21:44 +0300

    added «sdg» prefix to custom components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 @Component({
-  selector: 'app',
+  selector: 'sdg-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
