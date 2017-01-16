75218db79460efb4eed5604bb6302beeb7ac0da5
Roman Lukyanyuk <ramonak.luk@gmail.com>
2017-01-16 16:20:03 +0300

    setup webpack rules related to oauth page

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -64,7 +64,7 @@ module.exports = function (options) {
 
       new CopyWebpackPlugin([
         { from: 'src/assets/images', to: 'assets/images' },
-        { from: 'src/oauth.html', to: 'oauth.html' }
+        { from: 'src/assets/oauth.html', to: 'assets/oauth.html' }
       ]),
 
       new HtmlWebpackPlugin({
